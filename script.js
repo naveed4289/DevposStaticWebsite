f9ea5cf98f2b6191a9c1d9ee213603959eb7b51a
Naveed Ishtiaq <126350839+naveed4289@users.noreply.github.com>
2025-01-21 17:21:04 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// JavaScript to show a message when the button is clicked
+document.getElementById('showMessageBtn').addEventListener('click', function() {
+    alert('Thanks for visiting our website!');
+});
